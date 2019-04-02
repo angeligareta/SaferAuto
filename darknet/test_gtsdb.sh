698dd78dbde668b2ee457df272ed43074e0485eb
Angel Igareta <angel@igareta.com>
2019-04-02 17:51:50 +0100

    Added a modificated fork of the AlexeyAB/darknet repository.

---
+++ darknet/test_gtsdb.sh
@@ -0,0 +1,4 @@
+./darknet detector map cfg/gtsdb/gtsdb.data cfg/gtsdb/sl-yolov3-spp.cfg weights/weights-gtsdb/sl-yolov3-spp_last.weights
+
+
+
---
+++ darknet/train_gtsdb.sh
@@ -0,0 +1,4 @@
+./darknet detector train -map cfg/gtsdb/gtsdb.data cfg/gtsdb/sl-yolov3-spp.cfg weights/darknet53.conv.74
+
+
+

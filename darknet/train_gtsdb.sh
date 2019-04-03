fb5921397ecabf366fa3d245c4d6bd5b18edc3ff
angeligareta <alu0100967111@ull.edu.es>
2019-04-03 20:08:51 +0100

    Training a new model changing some parameters from the net.

--- darknet/train_gtsdb.sh
+++ darknet/train_gtsdb.sh
@@ -1,4 +1,4 @@
-./darknet detector train -map cfg/gtsdb/gtsdb.data cfg/gtsdb/sl-yolov3-spp.cfg weights/darknet53.conv.74
+./darknet detector train -map cfg/gtsdb/gtsdb.data cfg/gtsdb/sl-yolov3-spp.cfg weights/yolov3.conv.81
 
 
 

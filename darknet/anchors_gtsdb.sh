50bed9c7817b47532808cc4976ce8414b5f8694e
angeligareta <alu0100967111@ull.edu.es>
2019-04-06 15:13:28 +0100

    Modified model to train with Beligium TS dataset.

--- darknet/anchors_gtsdb.sh
+++ darknet/anchors_gtsdb.sh
@@ -1,1 +1,1 @@
-./darknet detector calc_anchors cfg/gtsdb-joinned/gtsdb.data -num_of_clusters 9 -width 416 -height 416
+./darknet detector calc_anchors cfg/btsdb/btsdb.data -num_of_clusters 9 -width 416 -height 416

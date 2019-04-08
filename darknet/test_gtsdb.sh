5927cf1e9e92c622da5d3b4c6eb2317080e068aa
angeligareta <alu0100967111@ull.edu.es>
2019-04-08 19:21:16 +0100

    Modified files for new training with Beligum TS dataset.

--- darknet/test_gtsdb.sh
+++ darknet/test_gtsdb.sh
@@ -1,4 +1,4 @@
-./darknet detector map cfg/gtsdb/gtsdb.data cfg/gtsdb/sl-yolov3-spp.cfg weights/weights-gtsdb/sl-yolov3-spp_last.weights
+./darknet detector map cfg/btsdb/btsdb.data cfg/btsdb/yolov3-spp.cfg weights/weights-btsdb/yolov3-spp_last.weights
 
 
 

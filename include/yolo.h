4e311d206212ee0c8b8a226eaef1115033281cbe
angeligareta <alu0100967111@ull.edu.es>
2019-04-27 23:12:16 +0100

    Documented files for YOLO detection.
    Former-commit-id: c8386f46fc9d7a37dd7316d319aa3b669686c1a2

--- include/yolo.h
+++ include/yolo.h
@@ -9,6 +9,7 @@
 #include <thread>
 #include <QSettings>
 #include <QString>
+#include <QCoreApplication>
 
 #define OPENCV
 // #define TRACK_OPTFLOW
@@ -21,84 +22,172 @@
 
 class DetectionWindow;
 
-/*#ifndef MAINWINDOW_H
-#include "detectionwindow.h"
-#include <opencv2/opencv.hpp> // C++s
-#endif*/
-
+// CONFIG INI TAGS FOR FILES USED IN SAFER AUTO
 const QString CFG_FILE = "cfg_file";
 const QString NAMES_FILE = "names_file";
 const QString WEIGHTS_FILE = "weights_file";
 const QString INPUT_FILE = "input_file";
 const QString CONFIG_FILE = "config.ini";
 
+// DEFAULT PATHS FOR FILES USED IN SAFER AUTO
 const QString CFG_FILE_DEFAULT_PATH = "./darknet/cfg/edb/yolov3-spp.cfg";
 const QString NAMES_FILE_DEFAULT_PATH = "./darknet/cfg/edb/edb.names";
 const QString WEIGHTS_FILE_DEFAULT_PATH = "./darknet/weights/edb/yolov3-spp_4000.weights";
 const QString INPUT_FILE_DEFAULT_PATH = "./res/media/test-video-light.mp4";
 
+// Box color for the object detection boxes.
+const cv::Scalar BOX_COLOR = cv::Scalar(60, 160, 260);
+
+/**
+ * @brief The YOLO class contains all the methods for detecting the input media by using the YOLO C++ API,
+ * it draw boxes on the input and show the results in the Detection Window interface.
+ */
 class YOLO {
+
 private:
+    /**
+     * @brief settings_ saves the current cfg, names, weights and input file paths in a configuration file
+     * and update it when the values are changed.
+     */
     QSettings* settings_ = new QSettings(CONFIG_FILE, QSettings::NativeFormat);
-    const cv::Scalar BOX_COLOR = cv::Scalar(60, 160, 260);
+    /**
+     * @brief exit_signal_ is only activated when the user has pressed the canceled button in the detection window,
+     * it stops the detection process.
+     */
+    bool exit_signal_ = false;
 
-public:    
+public:
+    /**
+     * @brief YOLO default constructor.
+     *
+     * @param cfgFile
+     * @param namesFile
+     * @param weightsFile
+     * @param filename
+     */
     YOLO();
-    YOLO(std::string cfgFile, std::string namesFile, std::string weightsFile, std::string filename);
 
+    /**
+     * @brief drawBoxes adds a bounding box for every detected object in the frame.
+     *
+     * @param mat_img
+     * @param result_vec
+     * @param obj_names
+     * @param window
+     * @return
+     */
     cv::Mat drawBoxes(cv::Mat mat_img, std::vector<bbox_t> result_vec, std::vector<std::string> obj_names, DetectionWindow* window);
+
+    /**
+     * @brief addDetectedElement adds the detected element to the detection window for visualizing them separated.
+     *
+     * @param detected_element
+     * @param element_class
+     * @param window
+     */
+    void addDetectedElement(cv::Mat detected_element, std::string element_class, DetectionWindow* window);
+
+    /**
+     * @brief showResult sends the result to the detection window to display it..
+     *
+     * @param result_vec
+     * @param obj_names
+     * @param window
+     */
     void showResult(std::vector<bbox_t> const result_vec, std::vector<std::string> const obj_names, DetectionWindow *window);
+
+    /**
+     * @brief getObjectNamesFromFile returns a list of strings representing every line
+     * in the text file received by parameter.
+     *
+     * @param filename
+     * @return
+     */
     std::vector<std::string> getObjectNamesFromFile(const std::string filename);
 
+    /**
+     * @brief processInputFile depending on the file received as input,
+     * start processing a video or an image file.
+     *
+     * @param window
+     */
     void processInputFile(DetectionWindow *window);
+
+    /**
+     * @brief processVideoFile uses the YOLO C++ API to process a video, calculating the frames per second.
+     *
+     * @param detector
+     * @param obj_names
+     * @param window
+     */
     void processVideoFile(Detector detector, std::vector<std::string> obj_names, DetectionWindow *window);
+
+    /**
+     * @brief processImageFile uses the YOLO C++ API to simply process an image.
+     *
+     * @param detector
+     * @param obj_names
+     * @param window
+     */
     void processImageFile(Detector detector, std::vector<std::string> obj_names, DetectionWindow *window);
 
-    // GETTERS
+// GETTERS
     /**
      * @brief Getter of configuration file for Darknet.
      * @return
      */
     std::string getCfgFile() const;
+
     /**
      * @brief Getter of names file for Darknet.
      * @return
      */
     std::string getNamesFile() const;
+
     /**
      * @brief Getter of weights file for Darknet.
      * @return
      */
     std::string getWeightsFile() const;
+
     /**
      * @brief Getter of input file for Darknet detection.
      * @return
      */
     std::string getInputFile() const;
 
-    // SETTERS
+// SETTERS
     /**
      * @brief Setter of configuration file for Darknet.
      * @param value
      */
     void setCfgFile(const std::string &value);
+
     /**
      * @brief Setter of names file for Darknet.
      * @param value
      */
     void setNamesFile(const std::string &value);
+
     /**
      * @brief Setter of weights file for Darknet.
      * @param value
      */
     void setWeightsFile(const std::string &value);
+
     /**
      * @brief Setter of input file for Darknet detection.
      * @param value
      */
     void setInputFile(const std::string &value);
+
+    /**
+     * Updates exit signal to stop yolo.
+     * @brief setExit_signal
+     * @param value
+     */
+    void setExitSignal();
 };
 
 #endif // YOLO_H
 #include "detectionwindow.h"
-
--- src/yolo.cpp
+++ src/yolo.cpp
@@ -2,39 +2,32 @@
 
 YOLO::YOLO(){}
 
-YOLO::YOLO(std::string cfg_file_, std::string names_file_, std::string weights_file_, std::string filename)
-{
-    setCfgFile(cfg_file_);
-    setNamesFile(names_file_);
-    setWeightsFile(weights_file_);
-    setInputFile(filename);
-}
-
 cv::Mat YOLO::drawBoxes(cv::Mat mat_img, std::vector<bbox_t> result_vec, std::vector<std::string> obj_names, DetectionWindow* window) {
     for (auto &i : result_vec) {
-        int margin = 10;
         cv::Rect detection_roi = cv::Rect(i.x, i.y, i.w, i.h);
-        cv::Mat detected_sign = mat_img(detection_roi);
-        cv::resize(detected_sign, detected_sign, cv::Size(120, 120)); // Zoom detected sign
-        putText(detected_sign, obj_names[i.obj_id], cv::Point(25, 110), cv::FONT_HERSHEY_DUPLEX, 1, BOX_COLOR);
+        cv::Mat detected_element = mat_img(detection_roi);
+        std::string element_class = obj_names[i.obj_id];
+        cv::rectangle(mat_img, detection_roi, BOX_COLOR, 3); // Add bounding box of object to img
 
-        window -> displayDetectedElement(detected_sign);
-
-        cv::rectangle(mat_img, cv::Rect(i.x, i.y, i.w, i.h), BOX_COLOR, 3);
-
-        if(i.obj_id < obj_names.size())
-            putText(mat_img, obj_names[i.obj_id], cv::Point2f(i.x, i.y - 10), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, BOX_COLOR);
-        //if(i.track_id > 0)
-            //putText(mat_img, std::to_string(i.track_id), cv::Point2f(i.x + 5, i.y + 15), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, BOX_COLOR);
+        // if(i.obj_id < obj_names.size())
+            // putText(mat_img, element_class, cv::Point2f(i.x, i.y - 10), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, BOX_COLOR);
+        // if(i.track_id > 0)
+            // putText(mat_img, std::to_string(i.track_id), cv::Point2f(i.x + 5, i.y + 15), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, BOX_COLOR);
     }
 
     return mat_img;
 }
 
+void YOLO::addDetectedElement(cv::Mat detected_element, std::string element_class, DetectionWindow* window) {
+    cv::resize(detected_element, detected_element, cv::Size(120, 120)); // Zoom detected sign
+    putText(detected_element, element_class, cv::Point(25, 110), cv::FONT_HERSHEY_DUPLEX, 1, BOX_COLOR);
+    window -> displayDetectedElement(detected_element);
+}
+
 void YOLO::showResult(std::vector<bbox_t> const result_vec, std::vector<std::string> const obj_names, DetectionWindow* window) {
     for (auto &i : result_vec) {
         if (obj_names.size() > i.obj_id) {
-            std::cout << obj_names[i.obj_id] << " - ";
+            //std::cout << obj_names[i.obj_id] << " - ";
         }
         std::string info_text = "Last TS detected: " + obj_names[i.obj_id] + ". Probability: " + std::to_string(i.prob) + "%";
 
@@ -65,10 +58,9 @@ void YOLO::processVideoFile(Detector detector, std::vector<std::string> obj_name
     cv::Mat frame;
     detector.nms = 0.02f;
 
-    preview_boxes_t large_preview(100, 150, false);
-    const float kDetectionThreshold = 0.2f;
+    const float kDetectionThreshold = 0.5f;
 
-    for(cv::VideoCapture cap(getInputFile()); cap >> frame, cap.isOpened();) {
+    for(cv::VideoCapture cap(getInputFile()); cap.isOpened(), cap >> frame, cap.isOpened();) {
         auto begin = std::chrono::steady_clock::now();
         std::vector<bbox_t> result_vec = detector.detect(frame, kDetectionThreshold);
         result_vec = detector.tracking_id(result_vec);
@@ -79,18 +71,24 @@ void YOLO::processVideoFile(Detector detector, std::vector<std::string> obj_name
         std::string fps_text = "CURRENT FPS: " + std::to_string(fps) + " fps";
         window -> displayFPS(fps_text);
 
-        //large_preview.set(frame, result_vec);
+        // preview_boxes_t large_preview(100, 150, false);
+        // large_preview.set(frame, result_vec);
+        // large_preview.draw(frame, true);
+
         try {
             drawBoxes(frame, result_vec, obj_names, window);
         } catch (...) {
-
+            // TODO: Correct
         }
-
-        //large_preview.draw(frame, true);
-
         window -> displayImage(frame);
         window -> displayFPS(fps_text);
         showResult(result_vec, obj_names, window);
+
+        // For not blocking UI
+        QCoreApplication::processEvents();
+        if (exit_signal_) {
+            cap.release(); //TODO: Solve double free or corruption (!prev)
+        }
     }
 }
 
@@ -165,4 +163,9 @@ std::string YOLO::getWeightsFile() const
 std::string YOLO::getInputFile() const
 {
     return settings_->value(INPUT_FILE, INPUT_FILE_DEFAULT_PATH).toString().toStdString();
+}
+
+void YOLO::setExitSignal()
+{
+    exit_signal_ = true;
 }

67dad88776fb8fa63ef901201906de87ffce7bce
angeligareta <alu0100967111@ull.edu.es>
2019-05-03 23:56:47 +0100

    Modified input size yolo tiny.
    Former-commit-id: 306a20dd3e30ec391cceca16163587ecb98e246e

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,1 +1,1 @@
-./darknet/darknet detector map models/cfg/ere/ere.data models/cfg/ere/yolov3-tiny.cfg models/weights/ere-tiny/
+./darknet/darknet detector map models/cfg/ere/ere.data models/cfg/ere/yolov3-tiny.cfg models/weights/ere-tiny/yolov3-tiny_45000.weights
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,1 +1,1 @@
-./darknet/darknet detector train -map models/cfg/ere/ere.data models/cfg/ere/yolov3-tiny.cfg models/weights/yolov3-tiny.conv.15
+./darknet/darknet detector train -map models/cfg/ere/ere.data models/cfg/ere/yolov3-tiny.cfg models/weights/ere-tiny/yolov3-tiny_45000.weights

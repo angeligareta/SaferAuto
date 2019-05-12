cd2ad6693221a294d012d4352899c0d2f5e77859
angeligareta <alu0100967111@ull.edu.es>
2019-05-12 13:54:45 +0100

    Added new script for generating darknet library.
    Former-commit-id: edc4e03d4d77fb106d13849b718dc9c15e5412e8

---
+++ scripts/generate_darknet_lib.sh
@@ -0,0 +1,22 @@
+# Update submodules 
+git submodule update --init --recursive
+
+# Post-installation steps for cuda
+export PATH=/usr/local/cuda-10.0/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+
+# Activate GPU acceleration and OPENCV
+cd darknet
+sed -i 's/GPU=.*/GPU=1/' Makefile
+sed -i 's/CUDNN=.*/CUDNN=1/' Makefile
+sed -i 's/CUDNN_HALF=.*/CUDNN_HALF=1/' Makefile
+sed -i 's/OPENCV=.*/OPENCV=1/' Makefile
+sed -i 's/LIBSO=.*/LIBSO=1/' Makefile
+make
+
+# Save important compiled file
+mv libdarknet.so ../lib
+
+# Revert changes
+git stash
+make clean

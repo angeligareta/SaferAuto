52f76d78f1c5a359411e9c1448b40f83234fb9c1
angeligareta <alu0100967111@ull.edu.es>
2019-05-23 23:03:04 +0100

    Add new classes to eresl classifier.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,1 +1,1 @@
-./darknet/darknet detector map models/cfg/ere/ere.data models/cfg/ere/yolov3-tiny.cfg models/weights/ere-tiny/yolov3-tiny_72000.weights -points 0
+./darknet/darknet detector map models/cfg/ere/ere.data models/cfg/ere/yolov3-tiny.cfg models/weights/ere/yolov3-tiny_27000.weights -points 0
